839b8a3173ffa84376b7054b99ebb1b7b969352b
K <krseven@gmail.com>
2023-04-22 22:42:10 -0500

    connect UI buttons to logic, impl progress bar and reset timer button

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes = ["Soft": 5, "Medium": 8, "Hard": 12]
+    var secondsRemaining = 60
+    var totalSeconds = 60
+    var timer = Timer()
+    
+    @IBOutlet weak var titleText: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        timer.invalidate()
+        progressBar.progress = 0
+        titleText.text = "How do you like your eggs?"
+        
+    }
+    @IBAction func eggButton(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle! // Soft, Medium, Hard
+        
+        secondsRemaining = eggTimes[hardness]!
+        
+        totalSeconds = secondsRemaining
+        
+        // invalidate any existing timers from previous button clicks
+        timer.invalidate()
+        progressBar.progress = 0
+        
+        // scheduling a timer in repeat mode after each second.
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                             target: self,
+                                             selector: #selector(updateTimer),
+                                             userInfo: nil,
+                                             repeats: true)
+               
+    }
+    
+    @objc private func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds ...")
+           
+            
+            secondsRemaining -= 1
+            progressBar.progress = Float(totalSeconds - secondsRemaining) / Float(totalSeconds)
+            print(progressBar.progress)
+        } else {
+            titleText.text = "Done!"
+            progressBar.progress = 1
+            timer.invalidate()
+        }
+        
+    }
+    
+//    func printBoilTime(text: String) -> Void {
+//        switch (text) {
+//        case "Soft":
+//            print(5)
+//        case "Medium":
+//            print(8)
+//        case "Hard":
+//            print(12)
+//        default:
+//            print("error")
+//        }
+//    }
+    
+    func startTimer() {
+        
+    }
 }
